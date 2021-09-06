e88263a764f5ef02128d878dc85b8b2e53d0afed
Moira Leon <moira.leon2@gmail.com>
2021-09-06 13:00:31 -0600

    notes

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 // run npm i to install package.json
 // run npm init y to instal package-lock.json
 // run nodemon to monitor the success of your javascript in real time
+const express= require('express');
+const path = require('path');
+const app =express();
+app.use(express.json());
+app.use(express.static('public'))
+
+
 
